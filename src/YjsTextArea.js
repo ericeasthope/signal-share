5fb35bf3948ce148835facdf9e8794171c3c1f66
Eric Easthope <eric.easthope@me.com>
2019-05-10 01:56:26 -0700

    Fix WebSocket provider

--- src/YjsTextArea.js
+++ src/YjsTextArea.js
@@ -6,7 +6,7 @@ import { TextareaBinding } from "yjs/bindings/textarea";
 export default class YjsTextArea extends Component {
     componentDidMount() {
         // const provider = new WebsocketProvider("ws://localhost:3000"); // change to wss://yjs-react-example.herokuapp.com/
-        const provider = new WebsocketProvider("wss://signalshare.herokuapp.com/");
+        const provider = new WebsocketProvider("wss://signal-share.herokuapp.com/");
         const ydocument = provider.get("textarea");
         const type = ydocument.define("textarea", YText);
         new TextareaBinding(type, this.refs.share);
