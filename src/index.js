ff73f0dbc1dbb3bf64ce24e0268f7dd5bc1e3a4f
Eric Easthope <eric.easthope@me.com>
2021-03-02 19:44:03 -0800

    Replace frontend with interaction demo

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as Y from "yjs";
+import { WebrtcProvider } from "y-webrtc";
+import * as d3 from "d3";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ydoc = new Y.Doc();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const provider = new WebrtcProvider("testest", ydoc, {
+  signaling: ["wss://whispering-lowlands-96095.herokuapp.com"],
+});
 
-// import registerServiceWorker from './registerServiceWorker';
-// registerServiceWorker();
+// const yarray = ydoc.get('array', Y.Array);
+const yarray = ydoc.get("testest", Y.Array);
+
+provider.on("synced", (synced) => {
+  // NOTE: This is only called when a different browser connects to this client
+  // Windows of the same browser communicate directly with each other
+  // Although this behavior might be subject to change.
+  // It is better not to expect a synced event when using y-webrtc
+  console.log("synced!", synced);
+});
+
+yarray.observeDeep(() => {
+  // console.log("yarray updated: ", yarray.toJSON());
+  console.log(".");
+  d3.select("body")
+    .transition()
+    .style("background-color", "#add8e6")
+    .duration(250)
+    .transition()
+    .duration(1000)
+    .style("background-color", "#000");
+});
+
+window.pushArray = () => {
+  yarray.push([""]);
+};
+
+d3.select("body")
+  .transition()
+  .style("background-color", "#333")
+  .duration(250)
+  .transition()
+  .duration(1000)
+  .style("background-color", "#000");
